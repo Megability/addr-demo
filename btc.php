fc0482c9455ac87dbdc1995ddacfc8a9d896467c
Megability <baisiye@gmail.com>
2023-12-25 00:11:49 +0800

    Taproot

--- btc.php
+++ btc.php
@@ -11,6 +11,10 @@ use BitWasp\Bitcoin\Key\Deterministic\HierarchicalKey;
 use BitWasp\Bitcoin\Script\P2shScript;
 use BitWasp\Bitcoin\Script\ScriptFactory;
 
+//https://github.com/andkom/php-bitcoin-address
+use AndKom\Bitcoin\Address\Output\OutputFactory;
+use AndKom\Bitcoin\Address\Taproot;
+
  //生成助记词
 function createMnemonicWord(){
     // Bip39
@@ -23,6 +27,7 @@ function createMnemonicWord(){
     // 通过随机数生成助记词
     $mnemonic = $bip39->entropyToMnemonic($entropy);
     // 输出助记词
+    echo "mnemonic: " . $mnemonic . PHP_EOL;
     return $mnemonic;
 }
 
@@ -36,7 +41,7 @@ function createBtcAddress($mnemonicWord){
     //$hardened = $master->derivePath("44'/0'/0'/0/".$offset);    //44的含义：https://github.com/bitcoin/bips
     //echo 'WIF: ' . $hardened->getPrivateKey()->toWif() . PHP_EOL;		  //私钥
     
-    $pss = [44,49,84];//BIP44/49/84/86：规定 4 种标准的推导路径，地址分别是 1 、3 、bc1q 、bc1p 开头，脚本类型是 P2PKH 、P2SH-P2WPKH 、P2WPKH 、P2TR
+    $pss = [44,49,84,86];//BIP44/49/84/86：规定 4 种标准的推导路径，地址分别是 1 、3 、bc1q 、bc1p 开头，脚本类型是 P2PKH 、P2SH-P2WPKH 、P2WPKH 、P2TR
     foreach($pss as $p){
         purpose($seed, $p);
     }
@@ -46,12 +51,28 @@ function getScriptPubKey(HierarchicalKey $key, $purpose)
 {
     switch ($purpose) {
         case 44:
+            $address = OutputFactory::p2pkh(hex2bin($key->getPublicKey()->getPubKeyHash()->getHex()))->address();
+            var_dump($address);
+
             return ScriptFactory::scriptPubKey()->p2pkh($key->getPublicKey()->getPubKeyHash());
         case 49:
+            $factory = new OutputFactory();
+            $p2wpkh = $factory->p2wpkh(hex2bin($key->getPublicKey()->getPubKeyHash()->getHex()));
+            $address = $factory->p2sh($p2wpkh)->address();
+            var_dump($address);
+
             $rs = new P2shScript(ScriptFactory::scriptPubKey()->p2wkh($key->getPublicKey()->getPubKeyHash()));
             return $rs->getOutputScript();
         case 84:
+            $address = OutputFactory::p2wpkh(hex2bin($key->getPublicKey()->getPubKeyHash()->getHex()))->address();
+            var_dump($address);
+
             return ScriptFactory::scriptPubKey()->p2wkh($key->getPublicKey()->getPubKeyHash());
+        case 86:
+            $taprootPubKey = Taproot::construct(hex2bin($key->getPublicKey()->getHex()));
+            $address = OutputFactory::p2tr($taprootPubKey)->address();
+            var_dump($address);
+            return ;
         default:
             throw new \InvalidArgumentException("Invalid purpose");
     }
@@ -89,11 +110,19 @@ function purpose($seed, $purpose){
     $xpub = $factory->fromExtended($purposePub);
 
     $addressCreator = new AddressCreator();
+
     $script0 = getScriptPubKey($xpub->derivePath("0/0"), $purpose);
-    echo "0/0: ".$addressCreator->fromOutputScript($script0)->getAddress().PHP_EOL;
+    if($purpose<86){
+        echo "0/0: ".$addressCreator->fromOutputScript($script0)->getAddress().PHP_EOL;
+    }
     echo "0/0-PrivateKey: ".$root->derivePath("{$purpose}'/0'/0'/0/0")->getPrivateKey()->toWif().PHP_EOL;
+    
+
     //$script1 = getScriptPubKey($xpub->derivePath("0/1"), $purpose);
-    //echo "0/1: ".$addressCreator->fromOutputScript($script1)->getAddress().PHP_EOL;
+    //if($purpose<86){
+    //    echo "0/1: ".$addressCreator->fromOutputScript($script1)->getAddress().PHP_EOL;
+    //}
+    //echo "0/1-PrivateKey: ".$root->derivePath("{$purpose}'/0'/0'/0/1")->getPrivateKey()->toWif().PHP_EOL;
 }
 
 $addr = createBtcAddress(createMnemonicWord());
